014bb01c99bc38f86da455a1e811fe88936e64ee
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-09-03 02:44:52 +0600

    Category Section Ok

--- index.js
+++ index.js
@@ -38,9 +38,66 @@ const loadCataoryAllNews = async(categoryID) => {
 }
 
 const displayCategoryAllNews = (allNews) => {
-
+    const newsContainer = document.getElementById('newsContainer');
+    const categoryItemNumber = document.getElementById('categoryItemNumber');
+    categoryItemNumber.innerHTML = `
+    ${allNews.length} items found for
+    `
+    console.log(categoryItemNumber);
+    newsContainer.innerText = "";
+    console.log(allNews);
     allNews.forEach(news => {
         console.log(news);
+        const newsID = document.createElement('div');
+        newsID.classList.add('row');
+        newsID.classList.add('news');
+        newsID.classList.add('shadow-lg');
+        newsID.innerHTML = `
+        <div class="col-lg-3 col-sm-12 px-4">
+                        <img class="img-fluid" src="${news.thumbnail_url}" alt="">
+                    </div>
+                    <div class="col-lg-9 col-sm-12 p-4 text-start">
+                        <div>
+                            <h4>${news.title}</h4>
+                            <p>${news.details.length > 338 ? news.details.slice(0,338)+"...":news.details  }</p>
+                        </div>
+                        </br></br>
+                        <div class="row">
+                            <div class="col-lg-3 col-sm-6 d-flex justify-content-center align-items-center">
+                                <div>
+                                    <img class="rounded-circle author-imag" src="${news.author.img}" alt="">
+                                </div>
+                                <div class="ms-2">
+                                    <p class="p-0 m-0 fw-bold">${news.author.name}</p>
+                                    <p class="p-0 m-0 fw-bold">${news.author.published_date}</p>
+                                </div>
+
+
+                            </div>
+                            <div class="col-lg-3 col-sm-6 d-flex justify-content-center align-items-center">
+                                <a href="" style="font-size:30px ;"> <i class="fa-solid fa-eye"></i></a>
+                                <p class=" fw-bolder fs-1 mx-1">${news.rating.number} M</p>
+
+
+                            </div>
+                            <div class="col-lg-3 col-sm-6  d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-star-half-stroke fs-4 px-2"></i>
+                                <i class="fa-regular fa-star fs-4 px-2"></i>
+                                <i class="fa-regular fa-star fs-4 px-2"></i>
+                                <i class="fa-regular fa-star fs-4 px-2"></i>
+                                <i class="fa-regular fa-star fs-4 px-2"></i>
+
+                            </div>
+                            <div class="col-lg-3 col-sm-6 d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-arrow-right fs-3 "></i>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+        `;
+        newsContainer.appendChild(newsID)
     })
 }
 
